53ab07fb066629a096ebe7994168f37d312c372c
baskar0814@gmail.com <baskar0814@gmail.com>
2026-08-17 18:06:47 +0000

    DATE

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -1,7 +1,13 @@
 with CTE as (
-    select modifieddate 
+    select 
+    TO_TIMESTAMP(modifieddate) AS modifieddate,
+    DATE(TO_TIMESTAMP(modifieddate)) AS DATE_modifieddate,
+    HOUR(TO_TIMESTAMP(modifieddate)) AS HOUR_modifieddate,
+    DAYNAME(TO_TIMESTAMP(modifieddate)) AS DAY_modifieddate,
+    MONTHNAME(TO_TIMESTAMP(modifieddate)) AS MONTH_modifieddate,
+    YEAR(TO_TIMESTAMP(modifieddate)) AS YEAR_modifieddate
     from
-    {{ source('PROJ_BASS', 'TRANSACTIONS') }}
+    {{ source('PROJ_DBT', 'TRANSACTIONS') }}
 )
 select * from CTE
 
